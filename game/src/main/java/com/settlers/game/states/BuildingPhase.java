53219966ca3d053b825abc832cf27d65cd51bf4c
christoffer-cj <stoffer.msc@gmail.com>
2024-10-06 19:19:32 +0200

    jeg glæjer meg

--- game/src/main/java/com/settlers/game/states/BuildingPhase.java
+++ game/src/main/java/com/settlers/game/states/BuildingPhase.java
@@ -13,16 +13,72 @@ public class BuildingPhase extends BaseState {
 
         if (!player.color().equals(building.color())) return false;
 
-        return false; // todo
+        switch (building.type()) {
+            case SETTLEMENT -> {
+                boolean hasEnoughBrick = game.getPlayer(player.color()).inventory().resources().get(Resource.BRICK) >= 1;
+                boolean hasEnoughGrain = game.getPlayer(player.color()).inventory().resources().get(Resource.GRAIN) >= 1;
+                boolean hasEnoughWool = game.getPlayer(player.color()).inventory().resources().get(Resource.WOOL) >= 1;
+                boolean hasEnoughLumber = game.getPlayer(player.color()).inventory().resources().get(Resource.LUMBER) >= 1;
+
+                boolean hasEnoughResources = hasEnoughBrick && hasEnoughGrain && hasEnoughWool && hasEnoughLumber;
+                if (!hasEnoughResources) return false;
+
+                if (!game.getBoard().addBuilding(position, building)) return false;
+
+                game.getPlayer(player.color()).inventory().resources().merge(Resource.BRICK, -1, Integer::sum);
+                game.getPlayer(player.color()).inventory().resources().merge(Resource.GRAIN, -1, Integer::sum);
+                game.getPlayer(player.color()).inventory().resources().merge(Resource.WOOL, -1, Integer::sum);
+                game.getPlayer(player.color()).inventory().resources().merge(Resource.LUMBER, -1, Integer::sum);
+
+                return true;
+            }
+            case CITY -> {
+                boolean hasEnoughGrain = game.getPlayer(player.color()).inventory().resources().get(Resource.GRAIN) >= 2;
+                boolean hasEnoughOre = game.getPlayer(player.color()).inventory().resources().get(Resource.ORE) >= 3;
+
+                boolean hasEnoughResources = hasEnoughGrain && hasEnoughOre;
+                if (!hasEnoughResources) return false;
+
+                if (!game.getBoard().addBuilding(position, building)) return false;
+
+                game.getPlayer(player.color()).inventory().resources().merge(Resource.GRAIN, -2, Integer::sum);
+                game.getPlayer(player.color()).inventory().resources().merge(Resource.ORE, -3, Integer::sum);
+
+                return true;
+            }
+            default -> {
+                return false;
+            }
+        }
     }
 
     @Override
     public boolean addRoad(Player player, Position position, Road road) {
-        return super.addRoad(player, position, road);
+        if (!game.getCurrentPlayer().equals(player)) return false;
+
+        if (!player.color().equals(road.color())) return false;
+
+        boolean hasEnoughBrick = game.getPlayer(player.color()).inventory().resources().get(Resource.BRICK) >= 1;
+        boolean hasEnoughLumber = game.getPlayer(player.color()).inventory().resources().get(Resource.LUMBER) >= 1;
+
+        boolean hasEnoughResources = hasEnoughBrick && hasEnoughLumber;
+        if (!hasEnoughResources) return false;
+
+        if (!game.getBoard().addRoad(position, road)) return false;
+
+        game.getPlayer(player.color()).inventory().resources().merge(Resource.BRICK, -1, Integer::sum);
+        game.getPlayer(player.color()).inventory().resources().merge(Resource.LUMBER, -1, Integer::sum);
+
+        return true;
     }
 
     @Override
     public boolean endTurn(Player player) {
-        return super.endTurn(player);
+        if (!game.getCurrentPlayer().equals(player)) return false;
+
+        game.nextPlayer();
+        game.setState(new RollForResources(game));
+
+        return true;
     }
 }
